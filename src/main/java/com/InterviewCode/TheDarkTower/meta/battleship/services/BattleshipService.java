75b583011f6d43c9ae4f62ac637bb1334df5f909
Andrew Alsberge <andrew@alsberge.email>
2024-02-12 13:57:57 -0600

    fill in stub and fix master
    MAKE MASTER PROTECTED

--- src/main/java/com/InterviewCode/TheDarkTower/meta/battleship/services/BattleshipService.java
+++ src/main/java/com/InterviewCode/TheDarkTower/meta/battleship/services/BattleshipService.java
@@ -5,9 +5,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class BattleshipService {
 
+    //quick and has a problem that R and/or C could be out of bounds for G
     public double getHitProbability(int R, int C, int[][] G) {
-        // Write your code here
-        return 0.0;
+        int targets = 0;
+        for (int rowIndex = 0; rowIndex < R; rowIndex++) {
+            for (int colIndex = 0; colIndex < C; colIndex++) {
+                if (G[rowIndex][colIndex] == 1) {
+                    targets++;
+                }
+            }
+        }
+        return (double) targets /(R*C);
     }
 
 }
