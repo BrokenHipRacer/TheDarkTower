39276c12c6590aaab359a5c2524d3023de726562
Andrew Alsberge <andrew@alsberge.email>
2024-02-12 13:44:09 -0600

    tdd and stub

---
+++ src/main/java/com/InterviewCode/TheDarkTower/meta/battleship/services/BattleshipService.java
@@ -0,0 +1,13 @@
+package com.InterviewCode.TheDarkTower.meta.battleship.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BattleshipService {
+
+    public double getHitProbability(int R, int C, int[][] G) {
+        // Write your code here
+        return 0.0;
+    }
+
+}
---
+++ src/test/java/com/InterviewCode/TheDarkTower/unit/meta/BattleshipServiceTest.java
@@ -0,0 +1,41 @@
+package com.InterviewCode.TheDarkTower.unit.meta;
+
+import com.InterviewCode.TheDarkTower.meta.battleship.services.BattleshipService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class BattleshipServiceTest {
+
+    @Autowired
+    BattleshipService battleshipService;
+
+    @Test
+    public void getHitProbabilitySampleCase1() {
+        int R = 2;
+        int C = 3;
+        int[][] G = new int[2][3];
+        G[0][0] = 0;
+        G[0][1] = 0;
+        G[0][2] = 1;
+        G[1][0] = 1;
+        G[1][1] = 0;
+        G[1][2] = 1;
+        assertEquals(battleshipService.getHitProbability(R,C,G), 0.50000000);
+    }
+
+    @Test
+    public void getHitProbabilitySampleCase2() {
+        int R = 2;
+        int C = 3;
+        int[][] G = new int[2][2];
+        G[0][0] = 1;
+        G[0][1] = 1;
+        G[1][0] = 1;
+        G[1][1] = 1;
+        assertEquals(battleshipService.getHitProbability(R,C,G), 1.00000000);
+    }
+}
